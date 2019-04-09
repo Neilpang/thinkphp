ffadd23d9c529023a76f9dc2064963b8547f1db3
neilpang <neil@neilpang.com>
2019-04-09 23:27:59 +0800

    add route

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 
 if [ "$(ls /var/www/html/tp5/application | wc -l)" = "0" ]; then
   cp -r /var/www/tp5/application/* /var/www/html/tp5/application
+  cp  /var/www/tp5/route/* /var/www/html/tp5/route
 fi
 
 exec /usr/local/bin/docker-php-entrypoint "$@"
---
+++ readme.md
@@ -0,0 +1,10 @@
+volumes:
+
+```
+/var/www/html/tp5/application
+
+/var/www/html/tp5/route
+
+/var/www/html/tp5/public/static
+
+```
