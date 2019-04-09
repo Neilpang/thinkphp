3d56117298d4093e0ed115da465c72aeaf18a46a
neilpang <neil@neilpang.com>
2019-04-09 23:15:22 +0800

    init application

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 
-if [ "$(ls /var/www/html/ | wc -l)" = "0" ]; then
-  cp -r /var/www/tp5 /var/www/html/tp5
+if [ "$(ls /var/www/html/tp5/application | wc -l)" = "0" ]; then
+  cp -r /var/www/tp5/application/* /var/www/html/tp5/application
 fi
 
 exec /usr/local/bin/docker-php-entrypoint "$@"
